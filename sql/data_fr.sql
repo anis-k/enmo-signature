0468e92bc8f45e32eaec3492b9d8210ce6a00aed
Alex ORLUC <alex.orluc@maarch.org>
2018-11-09 11:13:56 +0100

    add loop data fr

--- sql/data_fr.sql
+++ sql/data_fr.sql
@@ -25,5 +25,19 @@ INSERT INTO status (reference, label) VALUES ('VAL', 'Validé');
 INSERT INTO status (reference, label) VALUES ('REF', 'Refusé');
 
 /* Tests */
-INSERT INTO main_documents (reference, subject, doc_date, status, priority, sender, sender_entity, processing_user, recipient, creation_date) VALUES ('2018/A/1', 'Mon Courrier', CURRENT_TIMESTAMP, 2, 'Urgent', 'Oliver Queen', 'QE', 1, 'Barry Allen', CURRENT_TIMESTAMP);
-INSERT INTO adr_main_documents (main_document_id, type, path, filename, fingerprint) VALUES (1, 'DOC', 'tests/', 'test.pdf', 'toto');
+TRUNCATE TABLE main_documents;
+TRUNCATE TABLE adr_main_documents;
+TRUNCATE TABLE attachments;
+TRUNCATE TABLE adr_attachments;
+DO $$
+BEGIN
+FOR r in 1..500 LOOP
+INSERT INTO main_documents (id, reference, subject, doc_date, status, priority, sender, sender_entity, processing_user, recipient, creation_date) VALUES (r, '2018/A/' || r, 'Mon Courrier ' || r, CURRENT_TIMESTAMP, 2, 'Urgent', 'Oliver Queen', 'QE', 1, 'Barry Allen', CURRENT_TIMESTAMP);
+INSERT INTO adr_main_documents (main_document_id, type, path, filename) VALUES (r, 'DOC', 'tests/', 'test.pdf');
+
+INSERT INTO attachments (id, main_document_id, reference, subject, creation_date) VALUES (r, r, '2018/PJ/' || r, 'PJ 1', CURRENT_TIMESTAMP);
+
+INSERT INTO adr_attachments (attachment_id, type, path, filename) VALUES (r, 'ATTACH', 'tests/', 'test_pj_1.pdf');
+END LOOP;
+END;
+$$;
