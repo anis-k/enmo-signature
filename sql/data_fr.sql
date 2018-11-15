6b165541701d849c0147feac9956f831459ac6b5
florian.azizian <florian.azizian@maarch.org>
2018-11-15 12:06:41 +0100

    update angular + add ccornillac

--- sql/data_fr.sql
+++ sql/data_fr.sql
@@ -4,6 +4,7 @@
 TRUNCATE TABLE users;
 ALTER SEQUENCE users_id_seq RESTART WITH 1;
 INSERT INTO users (email, password, firstname, lastname, mode) VALUES ('jjane@maarch.com', '$2y$10$C.QSslBKD3yNMfRPuZfcaubFwPKiCkqqOUyAdOr5FSGKPaePwuEjG', 'Jenny', 'JANE', 'standard');
+INSERT INTO users (email, password, firstname, lastname, mode) VALUES ('ccornillac@maarch.com', '$2y$10$C.QSslBKD3yNMfRPuZfcaubFwPKiCkqqOUyAdOr5FSGKPaePwuEjG', 'Clovis', 'CORNILLAC', 'rest');
 
 ------------
 --DOCSERVERS
