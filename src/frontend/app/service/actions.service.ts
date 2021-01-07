2af1b34517a7a1e5887d8949ed04d1f83f589ac6
Alex ORLUC <alex.orluc@maarch.org>
2021-01-07 17:11:43 +0100

    FIX #15116 TIME 2 fix pos Y date

--- src/frontend/app/service/actions.service.ts
+++ src/frontend/app/service/actions.service.ts
@@ -94,7 +94,7 @@ export class ActionsService {
                                 'width': date.width,
                                 'height': date.height,
                                 'positionX': date.positionX,
-                                'positionY': date.positionY + 0.29,
+                                'positionY': date.positionY,
                                 'type': 'SVG',
                                 'page': index,
                             }
