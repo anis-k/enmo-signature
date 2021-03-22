be60263bcde789c4a583cc837f01383282c6c952
florian.azizian <florian.azizian@maarch.org>
2021-03-22 18:22:29 +0100

    FEAT #16511 TIME 0:10 update delete_all_ressources

--- sql/delete_all_ressources.sql
+++ sql/delete_all_ressources.sql
@@ -6,8 +6,20 @@ ALTER SEQUENCE history_id_seq restart WITH 1;
 TRUNCATE TABLE main_documents;
 ALTER SEQUENCE main_documents_id_seq restart WITH 1;
 
+TRUNCATE TABLE adr_main_documents;
+ALTER SEQUENCE adr_main_documents_id_seq restart WITH 1;
+
 TRUNCATE TABLE attachments;
 ALTER SEQUENCE attachments_id_seq restart WITH 1;
 
-TRUNCATE TABLE users_signatures;
-ALTER SEQUENCE users_signatures_id_seq restart WITH 1;
+TRUNCATE TABLE adr_attachments;
+ALTER SEQUENCE adr_attachments_id_seq restart WITH 1;
+
+TRUNCATE TABLE emails;
+ALTER SEQUENCE emails_id_seq restart WITH 1;
+
+TRUNCATE TABLE password_history;
+ALTER SEQUENCE password_history_id_seq restart WITH 1;
+
+TRUNCATE TABLE workflows;
+ALTER SEQUENCE workflows_id_seq restart WITH 1;
